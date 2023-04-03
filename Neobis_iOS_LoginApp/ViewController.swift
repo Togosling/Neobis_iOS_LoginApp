9f770d837baff3594e31ec2583451a4e3fd257a3
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-03 13:03:58 +0800

    first commit

---
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_LoginApp
+//
+//  Created by Тагай Абдылдаев on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
