227870e2419abe97a06498c0cb9399c72b273b79
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-03 13:35:40 +0800

    SnapKit and some UIElements

--- Neobis_iOS_LoginApp/ViewController.swift
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -7,11 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    let imageView: UIImageView = {
+       let iv = UIImageView()
+        iv.image = UIImage(named: "open-mail")
+        return iv
+        
+    }()
+    
+    let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "Username"
+        textField.textColor = .white
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 16
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "Password"
+        textField.textColor = .white
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 16
+        return textField
+    }()
+    
+    let loremIpsum: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum"
+        label.textColor = .white
+        return label
+    }()
+    
+    let SignInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign in", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.backgroundColor = .white
+        return button
+    }()
+    
+    let loremIpsum2: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum"
+        label.textColor = .white
+        return label
+    }()
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+    
     }
 
 
