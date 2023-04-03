9b938af44e97fed88e90260337fb10c6aa1e6722
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-03 18:13:15 +0800

    Final Design and TextField Handler

--- Neobis_iOS_LoginApp/ViewController.swift
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -13,10 +13,6 @@ class MainViewController: UIViewController {
     let imageView: UIImageView = {
         let iv = UIImageView()
         iv.image = UIImage(named: "open-mail")
-        iv.snp.makeConstraints { make in
-            make.width.equalTo(150)
-            make.height.equalTo(150)
-        }
         return iv
         
     }()
@@ -29,10 +25,6 @@ class MainViewController: UIViewController {
         textField.textAlignment = .center
         textField.backgroundColor = .gray
         textField.layer.cornerRadius = 32
-        textField.snp.makeConstraints { make in
-            make.width.equalTo(280)
-            make.height.equalTo(64)
-        }
         return textField
     }()
     
@@ -44,10 +36,6 @@ class MainViewController: UIViewController {
         textField.textAlignment = .center
         textField.backgroundColor = .gray
         textField.layer.cornerRadius = 32
-        textField.snp.makeConstraints { make in
-            make.width.equalTo(280)
-            make.height.equalTo(64)
-        }
         return textField
     }()
     
@@ -65,10 +53,6 @@ class MainViewController: UIViewController {
         button.setTitleColor(UIColor(red: 77/255, green: 77/255, blue: 77/255, alpha: 1), for: .normal)
         button.backgroundColor = .white
         button.layer.cornerRadius = 32
-        button.snp.makeConstraints { make in
-            make.width.equalTo(280)
-            make.height.equalTo(64)
-        }
         return button
     }()
     
@@ -86,29 +70,67 @@ class MainViewController: UIViewController {
     
         view.backgroundColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
         
-        let centerStackView = UIStackView(arrangedSubviews: [userNameTextField, passwordTextField,loremIpsum])
-        centerStackView.axis = .vertical
-        centerStackView.spacing = 32
-        centerStackView.alignment = .center
+        setupConstraints()
         
-        let botomStackView = UIStackView(arrangedSubviews: [signInButton, loremIpsum2])
-        botomStackView.axis = .vertical
-        botomStackView.spacing = 16
-        botomStackView.alignment = .center
-
+        signInButton.addTarget(self, action: #selector(handleSignIn), for: .touchUpInside)
         
-        let overallStackView = UIStackView(arrangedSubviews: [imageView, centerStackView, botomStackView])
-        overallStackView.axis = .vertical
-        overallStackView.spacing = 64
-        overallStackView.alignment = .center
-
-        
-        view.addSubview(overallStackView)
-        overallStackView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
     }
-
-
+    
+    @objc func handleSignIn() {
+        print("Usernam: \(userNameTextField.text ?? ""), Password: \(passwordTextField.text ?? "")")
+    }
+    
+    fileprivate func setupConstraints() {
+        
+         var customOffset = 200
+         var customHeight = 64
+        
+        if self.view.frame.height < 700 {
+            customOffset = 50
+            customHeight = 48
+        }
+        
+        self.view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(customOffset)
+            make.width.equalTo(150)
+            make.height.equalTo(150)
+        }
+        self.view.addSubview(userNameTextField)
+        userNameTextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(imageView.snp.bottom).offset(customHeight)
+            make.width.equalTo(280)
+            make.height.equalTo(customHeight)
+        }
+        
+        self.view.addSubview(passwordTextField)
+        passwordTextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(userNameTextField.snp.bottom).offset(20)
+            make.width.equalTo(280)
+            make.height.equalTo(customHeight)
+        }
+        self.view.addSubview(loremIpsum)
+        loremIpsum.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
+        }
+        self.view.addSubview(signInButton)
+        signInButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(loremIpsum.snp.bottom).offset(customHeight)
+            make.width.equalTo(280)
+            make.height.equalTo(customHeight)
+            
+        }
+        self.view.addSubview(loremIpsum2)
+        loremIpsum2.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(signInButton.snp.bottom).offset(20)
+        }
+        
+    }
 }
 
