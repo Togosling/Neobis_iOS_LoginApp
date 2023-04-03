a048d9ea7395904b05a7b61d5cf5b56d33b23240
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-03 15:05:44 +0800

    Draft

--- Neobis_iOS_LoginApp/ViewController.swift
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -6,31 +6,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
     
     let imageView: UIImageView = {
-       let iv = UIImageView()
+        let iv = UIImageView()
         iv.image = UIImage(named: "open-mail")
+        iv.snp.makeConstraints { make in
+            make.width.equalTo(150)
+            make.height.equalTo(150)
+        }
         return iv
         
     }()
     
     let userNameTextField: UITextField = {
         let textField = UITextField()
-        textField.text = "Username"
+        textField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        textField.font = .systemFont(ofSize: 20)
         textField.textColor = .white
+        textField.textAlignment = .center
         textField.backgroundColor = .gray
-        textField.layer.cornerRadius = 16
+        textField.layer.cornerRadius = 32
+        textField.snp.makeConstraints { make in
+            make.width.equalTo(280)
+            make.height.equalTo(64)
+        }
         return textField
     }()
     
     let passwordTextField: UITextField = {
         let textField = UITextField()
-        textField.text = "Password"
+        textField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        textField.font = .systemFont(ofSize: 20)
         textField.textColor = .white
+        textField.textAlignment = .center
         textField.backgroundColor = .gray
-        textField.layer.cornerRadius = 16
+        textField.layer.cornerRadius = 32
+        textField.snp.makeConstraints { make in
+            make.width.equalTo(280)
+            make.height.equalTo(64)
+        }
         return textField
     }()
     
@@ -41,11 +58,17 @@ class MainViewController: UIViewController {
         return label
     }()
     
-    let SignInButton: UIButton = {
+    let signInButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Sign in", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24)
+        button.setTitleColor(UIColor(red: 77/255, green: 77/255, blue: 77/255, alpha: 1), for: .normal)
         button.backgroundColor = .white
+        button.layer.cornerRadius = 32
+        button.snp.makeConstraints { make in
+            make.width.equalTo(280)
+            make.height.equalTo(64)
+        }
         return button
     }()
     
@@ -60,9 +83,30 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .black
     
+        view.backgroundColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
+        
+        let centerStackView = UIStackView(arrangedSubviews: [userNameTextField, passwordTextField,loremIpsum])
+        centerStackView.axis = .vertical
+        centerStackView.spacing = 32
+        centerStackView.alignment = .center
+        
+        let botomStackView = UIStackView(arrangedSubviews: [signInButton, loremIpsum2])
+        botomStackView.axis = .vertical
+        botomStackView.spacing = 16
+        botomStackView.alignment = .center
+
+        
+        let overallStackView = UIStackView(arrangedSubviews: [imageView, centerStackView, botomStackView])
+        overallStackView.axis = .vertical
+        overallStackView.spacing = 64
+        overallStackView.alignment = .center
+
+        
+        view.addSubview(overallStackView)
+        overallStackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 
 
